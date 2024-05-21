39c507be20a0f22239f21424661e3f72e6e350a0
Vishnu tyagi <106895072+Vishnutyagi@users.noreply.github.com>
2024-05-21 11:59:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 function mincost(arr)
 { 
 	arr.sort(function(a, b){return a - b});
-	let ans=0,temp=arr[0];
+	let ans=0;
+	let temp=arr[0];
 	for(let i=1; i<arr.length; i++){
 		ans+=(temp+arr[i]);
 		temp=ans;
